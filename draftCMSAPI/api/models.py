e92951212d06d7bf1ca2370bdfe709f90c067dd5
marioacc <mario.acc.15@gmail.com>
2018-08-10 15:38:35 -0400

    Models created. The models created for the next collections created: -Items -ItemStatus -User -UserRoles

--- draftCMSAPI/api/models.py
+++ draftCMSAPI/api/models.py
@@ -1,21 +1,40 @@
-from django.db import models
+from djongo import models
 
 
 # Create your models here.
+class User(models.Model):
+    """This class represents a user in the CMS"""
+    given_name = models.CharField(max_length=255,blank=False) 
+    family_name = models.CharField(max_length=255,blank=False)
+    role = models.PositiveSmallIntegerField(blank=False)
+    def __init__(self, given_name, family_name, role):
+        self.givenName = given_name
+        self.familyName = family_name
+        self.role = role
 
-class Item:
+class ItemStatus(models.Model):
+    name = models.CharField(max_length=255, blank=False, unique=True)
+
+class Item(models.Model):
+    """This class represents the most basic unit in CMS
+
+    """
+
+    title = models.CharField(max_length=255, blank=False, unique=True)
+    body = models.TextField(max_length=500,blank=True,unique=False)
+    author =models.ForeignKey(User,on_delete= models.CASCADE)
+    status = models.ForeignKey(ItemStatus,on_delete = models.CASCADE)
+    image = models.ImageField()
+
     def __init__(self, title, body, image, author, status):
-        print("This is the constructor method for Item")
         self.title = title
         self.body = body
         self.image = image
         self.author = author
         self.status = status
 
+class UserRoles(models.Model):
+    """This models maps the fields contained by every user role available
+    """
+    name = models.CharField(max_length=255, blank=False)
 
-class User:
-    def __init__(self, given_name, family_name, role):
-        print("This is the constructor method for User")
-        self.givenName = given_name
-        self.familyName = family_name
-        self.role = role
